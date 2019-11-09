7dbe629d7ba442a22e7cb0adbfa7f08b506035ae
SAUVAGE <emmanuel.sauvage@live>
2019-11-09 12:33:15 +0100

    create validator / manager / entity / slugger / controller

---
+++ src/Helper/Slugger.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helper;
+
+class Slugger
+{
+    public static function slugify(string $text): string
+    {
+        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = trim($text, '-');
+        $text = preg_replace('~-+~', '-', $text);
+        return strtolower($text);
+    }
+}
