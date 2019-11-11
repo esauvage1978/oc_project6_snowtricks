fdd80e777bb27afd9606b881243e8e7aad0ad868
SAUVAGE <emmanuel.sauvage@live>
2019-11-11 09:43:54 +0100

    create controller and template

---
+++ src/Controller/User/UserShowController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserShowController extends AbstractController
+{
+    /**
+     * @Route("/user/{id}", name="user_show", methods={"GET"})
+     *
+     * @param User $user
+     * @return Response
+     */
+    public function showAction(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
