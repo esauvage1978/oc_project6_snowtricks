5d797f40aa0bdaba44fc859c84a020f738cafe25
SAUVAGE <emmanuel.sauvage@live>
2019-11-12 18:54:24 +0100

    Create controller and template

---
+++ src/Controller/User/UserDeleteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Category;
+
+use App\Entity\User;
+use App\Service\UserManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/user")
+ */
+class UserDeleteController extends AbstractController
+{
+
+    /**
+     * @Route("/{id}", name="user_delete", methods={"DELETE"})
+     *
+     * @param Request $request
+     * @param User $user
+     * @param UserManager $categoryManager
+     * @return Response
+     */
+    public function deleteAction(Request $request, User $user, UserManager $userManager ): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+
+            $this->addFlash('success', 'L\{utilisateur est supprimé');
+            $userManager->remove($user);
+        }
+
+        return $this->redirectToRoute('user_index');
+    }
+}
---
+++ src/Service/UserManager.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Validator\UserValidator;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserManager
+{
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    /**
+     * @var UserValidator
+     */
+    private $validator;
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(ObjectManager $manager,
+                                UserValidator $validator,
+                                UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->manager = $manager;
+        $this->validator = $validator;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function update(User $user): bool
+    {
+        $this->initialiseUser($user);
+
+        if (!$this->validator->isValide($user)) {
+            return false;
+        }
+
+        $this->manager->persist($user);
+        $this->manager->flush();
+
+        return true;
+    }
+
+    public function initialiseUser(User $user)
+    {
+        $this->encodePassword($user);
+    }
+
+    public function encodePassword(User $user)
+    {
+        $plainPassword = $user->getPlainPassword();
+        if ($plainPassword) {
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword(
+                    $user,
+                    $plainPassword
+                ));
+        }
+    }
+
+    public function getError(User $entity)
+    {
+        return $this->validator->getErrors($entity);
+    }
+
+    /**
+     * @param User $user
+     */
+    public function remove(User $user)
+    {
+        $this->manager->remove($user);
+        $this->manager->flush();
+    }
+}
