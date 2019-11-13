6bbc8ad83f3b14facf0ca3ec646741d37d379463
SAUVAGE <emmanuel.sauvage@live>
2019-11-13 11:02:26 +0100

    Create controller and security

---
+++ src/Controller/User/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\User;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="user_login")
+     *
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="user_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+    }
+}
