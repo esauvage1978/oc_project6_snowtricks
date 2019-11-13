7ffdd3e782230e50ace0cbf0eb4e28e99e43a371
SAUVAGE <emmanuel.sauvage@live>
2019-11-13 07:26:05 +0100

    FIX requested changes

--- src/Controller/User/UserController.php
+++ src/Controller/User/UserController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserShowController extends AbstractController
+class UserController extends AbstractController
 {
     /**
      * @Route("/user/{id}", name="user_show", methods={"GET"})
